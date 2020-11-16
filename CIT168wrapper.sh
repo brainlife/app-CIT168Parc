98ff82460e181c62a357368c712c0c6354103619
Daniel Bullock <dnbulloc@iu.edu>
2020-11-16 18:34:52 -0500

    setup output dir, fixed wrapper fixed wrapper to take in lateralized parcellation and create correct output

--- CIT168wrapper.sh
+++ CIT168wrapper.sh
@@ -9,8 +9,8 @@ echo "$SUBBRAIN"
 
 #path to the template (binary) subcortical atlas
 TEMPLATEATLAS=/template/CIT168_T1w_700um.nii.gz
-#path to the probabilistic subcortical atlas
-PROBATLAS=/template/CIT168toMNI152_prob_atlas_bilat_1mm__merged.nii.gz
+#path to the probabilistic LATERALIZED subcortical atlas
+PROBATLAS=/template/CIT168toMNI152_prob_atlas_bilat_1mm__merged_Lat.nii.gz
 
 #standard ants wrapper from
 #https://github.com/jmtyszka/atlaskit/blob/master/tmp2ind_T1.sh
@@ -18,5 +18,5 @@ atlaskit/tmp2ind_T1.sh --T1ind=$SUBBRAIN --T1tmp=$TEMPLATEATLAS --Atmp=$PROBATLA
 
 #convert the output probabalistic (4D) subject-specific atlas to a binarized atlas (3D)
 #https://github.com/jmtyszka/atlaskit/blob/master/prob2det.sh
-atlaskit/prob2det.sh pA_tmp2ind.nii.gz T1w_tmp2ind.nii.gz .5
+atlaskit/prob2det.sh pA_tmp2ind.nii.gz output/parc.nii.gz .5
 
