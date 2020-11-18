a23bb27da4d4742d842f3aca5f04bde39bb654ee
Daniel Bullock <dnbulloc@iu.edu>
2020-11-18 18:12:03 -0500

    renamed main, manually created labels.json

--- CIT168wrapper.sh
+++ CIT168wrapper.sh
@@ -14,7 +14,7 @@ PROBATLAS=/template/CIT168toMNI152_prob_atlas_bilat_1mm__merged_Lat.nii.gz
 
 #standard ants wrapper from
 #https://github.com/jmtyszka/atlaskit/blob/master/tmp2ind_T1.sh
-atlaskit/tmp2ind_T1.sh --T1ind=$SUBBRAIN --T1tmp=$TEMPLATEATLAS --Atmp=$PROBATLAS --nthreads=1
+atlaskit/tmp2ind_T1.sh --T1ind=$SUBBRAIN --T1tmp=$TEMPLATEATLAS --Atmp=$PROBATLAS --nthreads=8
 
 #convert the output probabalistic (4D) subject-specific atlas to a binarized atlas (3D)
 #https://github.com/jmtyszka/atlaskit/blob/master/prob2det.sh
