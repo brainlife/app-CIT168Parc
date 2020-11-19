60b02f993f2b36214534b248d1720a4b63673248
Daniel Bullock <iisdanbul@gmail.com>
2020-11-19 11:17:22 -0500

    modified atlaskit pathing for docker use
    because submodules don't work on brainlife yet, relying on docker version of atlaskit.  A consequence of this is that the wrapper file will no longer work locally (because it's looking at the top of the directory structure for atlaskit)

--- CIT168wrapper.sh
+++ CIT168wrapper.sh
@@ -14,9 +14,9 @@ PROBATLAS=/template/CIT168toMNI152_prob_atlas_bilat_1mm__merged_Lat.nii.gz
 
 #standard ants wrapper from
 #https://github.com/jmtyszka/atlaskit/blob/master/tmp2ind_T1.sh
-atlaskit/tmp2ind_T1.sh --T1ind=$SUBBRAIN --T1tmp=$TEMPLATEATLAS --Atmp=$PROBATLAS --nthreads=8
+/atlaskit/tmp2ind_T1.sh --T1ind=$SUBBRAIN --T1tmp=$TEMPLATEATLAS --Atmp=$PROBATLAS --nthreads=8
 
 #convert the output probabalistic (4D) subject-specific atlas to a binarized atlas (3D)
 #https://github.com/jmtyszka/atlaskit/blob/master/prob2det.sh
-atlaskit/prob2det.sh pA_tmp2ind.nii.gz output/parc.nii.gz .5
+/atlaskit/prob2det.sh pA_tmp2ind.nii.gz output/parc.nii.gz .5
 
